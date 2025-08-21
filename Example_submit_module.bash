dbbeae1e30444b354c8db826b586026b79ed368a
HeatherKates <hkates@ufl.edu>
2025-08-21 13:07:08 -0400

    add absolute filepaths

--- Example_submit_module.bash
+++ Example_submit_module.bash
@@ -1,10 +1,10 @@
 module load nextflow
 module load apptainer
 
-nextflow run test_rmarkdown.nf \
+nextflow run /blue/cancercenter-dept/PIPELINES/REPORTING/rmarkdown_report_module/test_rmarkdown.nf \
   -profile singularity \
   --sample_id CX_lung_test_contrast \
   --params_file CX_lung_report_params.txt  \
-  --rmarkdown_container /blue/cancercenter-dept/PIPELINES/rmarkdown_report_module/rmarkdown_report.sif \
+  --rmarkdown_container /blue/cancercenter-dept/PIPELINES/REPORTING/rmarkdown_report_module/rmarkdown_report.sif \
   --rmarkdown_outdir results/reports \
   --log CX_test.log
